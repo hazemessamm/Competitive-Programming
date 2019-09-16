9174fa815795fa75438f0b91149395713c2ca788
Hazem Essam <hazemessamm@users.noreply.github.com>
2019-09-17 00:18:08 +0200

    Update CF9A.cpp

--- Codeforces/CF-A/CF9A.cpp
+++ Codeforces/CF-A/CF9A.cpp
@@ -6,7 +6,7 @@ int main()
 {
     int y = 0, w = 0;
     cin>>y>>w;
-    string probabilities[6] = {"1/6", "2/3", "1/2", "2/3", "5/6", "1/1"};
+    string probabilities[6] = {"1/6", "1/3", "1/2", "2/3", "5/6", "1/1"};
     int maximum = max(y, w);
     cout<<probabilities[6-maximum];
     return 0;
