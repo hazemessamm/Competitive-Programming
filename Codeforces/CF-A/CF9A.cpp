f067e1dca245acfc2bb78a36e56f2895436909ca
Hazem Essam <hazemessamm@users.noreply.github.com>
2019-09-17 00:16:04 +0200

    Create CF9A.cpp

---
+++ Codeforces/CF-A/CF9A.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int y = 0, w = 0;
+    cin>>y>>w;
+    string probabilities[6] = {"1/6", "2/3", "1/2", "2/3", "5/6", "1/1"};
+    int maximum = max(y, w);
+    cout<<probabilities[6-maximum];
+    return 0;
+}
