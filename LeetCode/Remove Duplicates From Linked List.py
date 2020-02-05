ce9cfa462281c806e0869b151bf955df66e7e52f
Hazem Essam <hazemessamm@users.noreply.github.com>
2020-02-05 02:28:32 +0200

    Create Remove Duplicates From Linked List.py

---
+++ LeetCode/Remove Duplicates From Linked List.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        if head is None or head.next is None:
+            return head
+        temp = head
+        while temp.next is not None:
+            if temp.val == temp.next.val:
+                temp.next = temp.next.next
+            else:
+                temp = temp.next
+        return head
+            
+            
