41384d03a4806d0dcf819df1b3787fe61947fcbb
Hazem Essam <hazemessamm@users.noreply.github.com>
2020-08-07 20:23:18 +0200

    Update Even-Fibonacci.py

--- ProjectEuler/Even-Fibonacci.py
+++ ProjectEuler/Even-Fibonacci.py
@@ -2,11 +2,12 @@
 fib_recorder = dict()
 
 
-
+#Initial fib values
 fib_recorder[1] = 1
 fib_recorder[2] = 1
 
-
+#Dynamic Programming is used here to memoize the N values for optimizing the function.
+#Some function calls will be free which means it will have O(1) because it will just retrive the value from the dictionary.
 def fib(n):
     if n == 0:
         return 0
@@ -23,14 +24,15 @@ def fib(n):
 
 
 final_sum = 0
-
 value = 1
 result = 0
+
+#To populate the fib_recorder dictionary until the value result produces a result which is less than or equal 4000000.
 while result <= 4000000:
     result = fib(value)
     value += 1
 
-
+#retriving the values to get the even values and sum them.
 for key, value in fib_recorder.items():
     if value % 2 == 0 and value <= 4000000:
             final_sum += value
